760def8bf86c92cac7bf080517e18519addd0154
Samuel-Labagnere <samuel.labagnere@gmail.com>
2024-11-14 17:32:16 +0100

    Added comments

--- projects/tweets/index.js
+++ projects/tweets/index.js
@@ -30,13 +30,26 @@ const tweets = [
     likes: 20
   }
 ];
+/**
+ * We will assume data is consistent but not necessarily composed of the exact elements we want
+ * to print, therefore we will define the expected structure of our outputs below
+ */
 const structure = ['content', 'likes', 'author'];
 
+/**
+ * Avoids redundancy in creating an element, assigning classes and appending the newly created
+ * element to a given parent
+ * 
+ * @param {String} tag
+ * @param {String[]} classToAdd
+ * @param {Element} parent Optional
+ * @returns {Element} Newly created element
+ */
 const createNewElement = (tag, classToAdd, parent = null) => {
   const element = document.createElement(tag);
   element.classList.add(classToAdd);
 
-  if (parent instanceof Element)
+  if (parent instanceof Element) // Avoid errors if parent is not a DOM Element (wether parent is defined or not)
   parent.appendChild(element);
 
   return element;
@@ -44,6 +57,11 @@ const createNewElement = (tag, classToAdd, parent = null) => {
 
 const ul = document.getElementById('tweets-list');
 
+/**
+ * Automatically creates and appends a tweet from the given data based on the expected structure
+ * 
+ * @param {Object} tweet Expected to be an object from the tweets data source
+ */
 const generateTweet = tweet => {
   const li = createNewElement('li', 'tweet', ul);
 
